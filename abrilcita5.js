3c6456472b09a2f16e390f94cd8f9b4b80a1df1c
abril zacarias <83786610+abrilzacarias@users.noreply.github.com>
2021-09-06 17:32:45 -0300

    Add files via upload

---
+++ abrilcita5.js
@@ -0,0 +1,45 @@
+// inicializa el ciclo - chequea si el ciclo debe continuar - actualiza el estado del ciclo después de cada iteración
+/* for (let numero = 1; numero <= 7; numero++) {
+    numero = numero + 1;
+    document.write(`#
+    `);} */
+
+// for (let line = "#"; line.length <= 7; line += "#"){
+//     document.write(`${line} <br>`);
+// }
+//fave
+/*for (let num = 0; num <= 100; num++) {
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log("FizzBuzz")
+        continue;
+    }
+    else if (num % 3 == 0) {
+      console.log("FIZZ");
+      continue;
+    }
+    else if (num % 5 == 0){
+        console.log("BUZZ")
+        continue;
+    }
+   
+    console.log(num)
+
+}*/
+
+
+/*let size = ;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board); */
